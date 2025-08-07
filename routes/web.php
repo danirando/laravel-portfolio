2c0f9db02b81fc74d6039332c5aac2b6f1b4dcb9
danirando <danielerando@tiscali.it>
2025-08-07 10:19:48 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Rotta per la home page
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// Rotte per la gestione del profilo utente
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Rotte dell'area amministrativa, protette da middleware 'auth' e 'verified'
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')             // tutte le rotte inizieranno con /admin
+    ->name('admin.')              // tutte le rotte avranno il nome che inizia con admin.
+    ->group(function () {
+        // /admin/index
+        Route::get('/index', [DashboardController::class, 'index'])->name('index');
+
+        // /admin/profile
+        Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');
+    });
+
+// Rotte di autenticazione (login, registrazione, password reset, ecc.)
+require __DIR__.'/auth.php';
