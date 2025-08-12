1ff8f85ffabf57cbd18c555d70437246cf24f8f9
danirando <danielerando@tiscali.it>
2025-08-12 08:54:57 +0200

    route edit and destroy

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -28,10 +28,45 @@
                <tr>
                    <td>{{ $project->name }}</td>
                    <td>{{ explode(' ', $project->period)[0] }}</td>
-                   <td><a href="{{ route('projects.show', $project->id)}}">Dettagli</a></td>
+                   <td class="d-flex justify-content-end gap-3">
+                    <a class="btn btn-primary" href="{{ route('projects.show', $project->id)}}">Dettagli</a>
+                    <a class="btn btn-warning" href="{{ route('projects.edit', $project->id)}}">Modifica</a>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                         Elimina
+                    </button>
+                   </td>
                 </tr>
                 @endforeach
         </tbody>
     </table></div>
+
+    <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<form action="{{ route('projects.destroy', $project)}}" method="POST">
+
+    @csrf
+    @method('DELETE')
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Sei sicuro di eliminare il progetto?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+        <button type="submit" class="btn btn-danger">Elimina</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
+
 
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
     <div class="container my-3">  
+        <a class="btn btn-primary my-3" href="{{ route("projects.index")}}">Home</a>
+        <h1>Titolo:</h1>
         <h1 class="mb-3">{{ $project->name}}</h1>
+        <h2>Periodo:</h2>
         <h2 class="mb-3">{{ explode(' ', $project->period)[0] }}</h2>
+        <h2>Descrizione:</h2>
         <p>{{ $project->description }}</p>
     </div>
 @endsection
